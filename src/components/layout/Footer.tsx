f04ea96b44ccee0a4e516dcc81d9a4c6e36b2484
Ramona Reinholdz <rare2400@student.miun.se>
2026-04-07 11:01:04 +0200

    lägger till footer-komponent och exporterar

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer>
+            Laborationsuppgift i kursen Fördjupad Frontend-utveckling.
+            &copy; 2026 - Ramona Reinholdz
+        </footer>
+    );
+}
+
+export default Footer;
